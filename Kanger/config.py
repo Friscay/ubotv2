601174779e054327f06a3216e58a9506248bb1ed
POV <101882690+Friscay@users.noreply.github.com>
2024-01-25 21:01:33 +0700

    ubotv2

---
+++ Kanger/config.py
@@ -0,0 +1,34 @@
+#==================================#
+#Hijacked by anonymous
+#==================================#
+
+DEVS = [1612671473]
+
+DEVICE_MODEL = "Kanger Assistant"
+
+API_ID = 2040
+
+API_HASH = "b18441a1ff607e10a989891a5462e627"
+
+BOT_TOKEN = "6547357892:AAFlAB6fuwwyxvYrYnkFY8wfX-7ZcHsCAsA"
+
+OWNER_ID = 1612671473
+
+DUMP_LOGS = -1001912692470
+
+USER_ID = 1612671473
+
+LOGS_MAKER_UBOT = -1001905144356
+
+SUPPORT_GROUP = -1001906038831
+
+BLACKLIST_CHAT = [
+    -1001473548283,-1001853283409,-1001704645461
+    ]
+
+RMBG_API = "3hYYMrCnpDQmAmE81u9aQAz2"
+
+COMMAND = ". ! ? : ;"
+PREFIX = COMMAND.split()
+
+MONGO_URL = "mongodb+srv://reon:reon@cluster0.h52cpph.mongodb.net/?retryWrites=true&w=majority"
